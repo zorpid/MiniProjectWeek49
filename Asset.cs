ffcaef75ff12efc839f73b1705d20b8090fa6f1b
zorpid <robin96.malmo@gmail.com>
2024-12-03 11:04:41 +0100

    Add project files.

---
+++ Asset.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Asset
+{
+    public Asset(string brand ,string modelName, DateTime buyDate, double price)
+    {
+        ModelName = modelName;
+        BuyDate = buyDate;
+        Price = price;
+        Brand = brand;
+    }
+
+    public string ModelName { get; set; }
+    public DateTime BuyDate { get; set; }
+    public double Price { get; set; }
+    public int EndOfLifeYears { get; set; } = 3; // 3 years
+    public string Brand {  get; set; }
+
+
+
+
+    public void PrintAssetDetails()
+    {
+        Console.WriteLine($"Model: {ModelName}");
+        Console.WriteLine($"Purchase Date: {BuyDate.ToShortDateString()}");
+        Console.WriteLine($"Price: {Price:C}");
+        Console.WriteLine($"End of Life: {EndOfLifeYears} years");
+        Console.WriteLine();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+List<Asset> assets = new List<Asset>();
+
+
+// Add mobile 
+assets.Add(new Mobile("iPhone", "9", new DateTime(2022, 4, 5), 999));
+assets.Add(new Mobile("Samsung", "9", new DateTime(2021, 7, 23), 850));
+assets.Add(new Mobile("Nokia", "9", new DateTime(2020, 11, 13), 300));
+// Addlaptops
+assets.Add(new Laptop("MacBook", "9", new DateTime(2022, 3, 15), 1200));
+assets.Add(new Laptop("Asus", "9", new DateTime(2021, 8, 10), 800));
+assets.Add(new Laptop("Lenovo", "9", new DateTime(2020, 12, 1), 700));
+
+
+
+
+
+Console.WriteLine("Asset Tracking Details:");
+Console.WriteLine("----------------------------------------------------------------");
+Console.WriteLine("Type".PadRight(15) + "Brand".PadRight(15) + "Model".PadRight(15) +
+                          "Purchase Date".PadRight(15)) ;
+
+
+
+foreach (var asset in assets)
+{
+    string assetType = asset.GetType().Name;  // Get the class name as the asset type
+
+    Console.WriteLine(
+        assetType.PadRight(15) +
+        asset.Brand.PadRight(15) +
+        asset.ModelName.PadRight(15) +
+        asset.BuyDate.ToString("MM/dd/yyyy").PadRight(15)
+    );
+}
+
+
+
+
+
+public class Laptop : Asset
+{
+    public Laptop(string brand, string modelName, DateTime buyDate, double price) : base(brand, modelName, buyDate, price)
+    {
+    }
+}
+
+public class Mobile : Asset
+{
+    public Mobile(string brand ,string modelName, DateTime purchaseDate, double price)
+        : base(brand, modelName, purchaseDate, price) { }
+}
+
+
+
